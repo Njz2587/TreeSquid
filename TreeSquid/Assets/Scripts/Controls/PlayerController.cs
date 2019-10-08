3b64ac68b2fc47a6148136744c2e6a564173a53a
Nicholas Zehl <njz2587@g.rit.edu>
2019-10-07 23:37:04 -0400

    Physics Joint Added + Script Cleaned

--- TreeSquid/Assets/Scripts/Controls/PlayerController.cs
+++ TreeSquid/Assets/Scripts/Controls/PlayerController.cs
@@ -1,40 +1,85 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Invector.CharacterController;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject playerChest;
-    public LayerMask stickMask;
-    public Texture2D progressBarEmpty, progressBarFull, whiteBoarder;
+    #region Variables & Inspector Options
+    [Header("Character Settings")]
+    public GameObject playerChest; //Bone The Player Will Be Launched From
+    public LayerMask stickMask; //Mask Of What Layers The Player Can Stick Too
 
-    public bool isStuck;
-    public GameObject stuckOnObject = null;
-    public GameObject previousStuckObject;
-    public int stuckOnPreviousLayer;
-    public float nudgePower;
-    private Vector3 defaultScale;
+    [Header("UI Settings")]
+    [Space(10)]
+    public Texture2D progressBarEmpty; //Texture for the empty charge meter
+    public Texture2D progressBarFull; //Texture for the charging meter
+    public Texture2D whiteBoarder; //Boarder around meter
+
+    #region Stored Data
+    private bool isStuck;
+    private int stuckOnPreviousLayer;
+    private GameObject stuckOnObject;
+    private GameObject previousStuckObject;
+
+    private float nudgePower;
     private float playerLaunchPower;
-    private float iconSize = 60, uiOffset = 6;
+
     private float MAX_FORCE = 22000 * 30;
     private float chargeIndex = 8000;
-    private float boarderRadius = 5f;
 
-    // Start is called before the first frame update
-    void Start()
+    private float iconSize = 60, uiOffset = 6;
+    private float boarderRadius = 5f;
+    #endregion
+    #endregion
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    private void Start()
     {
         nudgePower = MAX_FORCE / 18;
-        defaultScale = gameObject.transform.localScale;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    private void Update()
     {
         PlayerControls();
     }
 
+    /// <summary>
+    /// Draws Gizmo sphere for the stickpoint
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
 
+        if (stuckOnObject != null)
+        {
+            Gizmos.DrawSphere(stuckOnObject.transform.position, 0.2f);
+        }
+    }
+
+    /// <summary>
+    /// Draws Charge Meter
+    /// </summary>
+    private void OnGUI()
+    {
+        //Launch power
+        if (playerLaunchPower > 0)
+        {
+            Rect playerBarRect = new Rect(uiOffset + ((iconSize - iconSize / 3f) / 2), (Screen.height - iconSize - (uiOffset * 2)), iconSize / 3f, (playerLaunchPower / 4000) * -1);
+            GUI.DrawTexture(GetShadowRect(GetShadowRect(playerBarRect, boarderRadius, new Rect(1, 1, 1, 1)), 2f, new Rect(1, 1, 1, 1)), whiteBoarder, ScaleMode.StretchToFill, true, 10.0F, Color.white, 0, 0);
+            GUI.DrawTexture(GetShadowRect(playerBarRect, boarderRadius, new Rect(1, 1, 1, 1)), progressBarEmpty, ScaleMode.StretchToFill, true, 10.0F, Color.black, 0, 0);
+            GUI.DrawTexture(playerBarRect, progressBarFull, ScaleMode.StretchToFill, true, 10.0F, Color.red, 0, 0);
+        }
+    }
+
+    /// <summary>
+    /// Identifies if the player has collided with a layer it recognizes
+    /// </summary>
+    /// <param name="col"></param>
     private void OnCollisionEnter(Collision col)
     {
         if (!isStuck)
@@ -42,58 +87,62 @@ public class PlayerController : MonoBehaviour
             if (stickMask == (stickMask | 1 << col.gameObject.layer))
             {
                 isStuck = true;
-                GetComponent<Rigidbody>().isKinematic = true;
 
                 stuckOnObject = new GameObject();
                 stuckOnObject.name = "StickPoint";
+                stuckOnObject.AddComponent<SphereCollider>().isTrigger = true;
+                stuckOnObject.AddComponent<Rigidbody>().isKinematic = true;
 
+                #region Layer Config
                 stuckOnPreviousLayer = col.gameObject.layer;
-
                 if (col.gameObject.GetComponent<Rigidbody>())
                 {
                     col.gameObject.layer = 10;                 
                 }
-
                 previousStuckObject = col.gameObject;
+                #endregion
+
                 stuckOnObject.transform.position = col.contacts[0].point;
-                gameObject.transform.root.parent = stuckOnObject.transform;
+
+                #region Joint Config
+                CharacterJoint joint = gameObject.AddComponent<CharacterJoint>();
+                //gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                joint.connectedBody = stuckOnObject.GetComponent<Rigidbody>();
+                joint.autoConfigureConnectedAnchor = false;
+                //joint.enableProjection = true;
+                joint.anchor = transform.InverseTransformPoint(col.contacts[0].point);
+                joint.axis = new Vector3(1f, 1f, 1f);
+                joint.connectedAnchor = Vector3.zero;
+
+
+                SoftJointLimit softLimit = joint.lowTwistLimit;
+                softLimit.limit = 0;
+                softLimit.bounciness = 1f;
+                softLimit.contactDistance = 0;
+                joint.lowTwistLimit = softLimit;
+                joint.highTwistLimit = softLimit;
+                softLimit.limit = 360;
+                joint.swing1Limit = softLimit;
+                joint.swing2Limit = softLimit;
+                #endregion
+
+                gameObject.transform.parent = stuckOnObject.transform;
                 stuckOnObject.transform.parent = col.gameObject.transform;
             }
         }
     }
 
-
-    public IEnumerator ResetStick(float stickResetDelay)
+    /// <summary>
+    /// Players Controls
+    /// </summary>
+    private void PlayerControls()
     {
-        //Debug.Log("Attempting To Reset Stick...");
-        if (stuckOnObject)
-        {
-            if (stuckOnObject.transform.childCount == 0)
-            {
-                yield return new WaitForSeconds(stickResetDelay);
-                isStuck = false;
-                previousStuckObject.layer = stuckOnPreviousLayer;
-                Destroy(stuckOnObject);
-            }
-        }
-        else
-        {
-            yield return new WaitForSeconds(stickResetDelay);
-            isStuck = false;
-        }
-        //Debug.Log("Reset!");
-    }
-
-    void PlayerControls()
-    {
-        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        #region Nudge
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
         {
             string keyCode = Input.inputString;
 
-            //playerChest.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            GetComponent<Rigidbody>().isKinematic = false;
-            gameObject.transform.parent = null;
-            StartCoroutine(ResetStick(0.01f));
+            ReleaseStick();
 
             if (playerChest.GetComponent<Rigidbody>().velocity.magnitude < 10)
             {
@@ -115,7 +164,7 @@ public class PlayerController : MonoBehaviour
                 }
             }
         }
-       
+        #endregion
 
         #region Launch
         if (Input.GetMouseButton(0)) //left mouse down
@@ -131,10 +180,7 @@ public class PlayerController : MonoBehaviour
 
             if (isStuck)
             {
-                GetComponent<Rigidbody>().isKinematic = false;
-                gameObject.transform.parent = null;
-                //Debug.Log("Reset Stick...");
-                StartCoroutine(ResetStick(0.1f));
+                ReleaseStick();
             }
             playerChest.GetComponent<Rigidbody>().AddForce(playerChest.GetComponent<vThirdPersonInput>().PlayerOneCam.transform.forward * (playerLaunchPower));
             playerLaunchPower = 0;
@@ -159,30 +205,60 @@ public class PlayerController : MonoBehaviour
         #endregion
     }
 
-    void OnGUI()
+    #region Helper Methods
+    /// <summary>
+    /// Resets the player's ability to stick to walls
+    /// </summary>
+    /// <param name="stickResetDelay"></param>
+    /// <returns></returns>
+    private IEnumerator ResetStick(float stickResetDelay)
     {
-        //Launch power
-        if (playerLaunchPower > 0)
+        //Debug.Log("Attempting To Reset Stick...");
+        if (stuckOnObject)
         {
-            Rect playerBarRect = new Rect(uiOffset + ((iconSize - iconSize / 3f) / 2), (Screen.height - iconSize - (uiOffset * 2)), iconSize / 3f, (playerLaunchPower / 4000) * -1);
-            GUI.DrawTexture(GetShadowRect(GetShadowRect(playerBarRect, boarderRadius, new Rect(1, 1, 1, 1)), 2f, new Rect(1, 1, 1, 1)), whiteBoarder, ScaleMode.StretchToFill, true, 10.0F, Color.white, 0, 0);
-            GUI.DrawTexture(GetShadowRect(playerBarRect, boarderRadius, new Rect(1, 1, 1, 1)), progressBarEmpty, ScaleMode.StretchToFill, true, 10.0F, Color.black, 0, 0);
-            GUI.DrawTexture(playerBarRect, progressBarFull, ScaleMode.StretchToFill, true, 10.0F, Color.red, 0, 0);
-        }           
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-
-        if (stuckOnObject != null)
-        {
-            Gizmos.DrawSphere(stuckOnObject.transform.position, 0.2f);
+            if (stuckOnObject.transform.childCount == 0)
+            {
+                yield return new WaitForSeconds(stickResetDelay);
+                if (gameObject.transform.parent == null)
+                {
+                    isStuck = false;
+                    previousStuckObject.layer = stuckOnPreviousLayer;
+                    Destroy(stuckOnObject);
+                }
+            }
         }
+        else
+        {
+            yield return new WaitForSeconds(stickResetDelay);
+            isStuck = false;
+        }
+        //Debug.Log("Reset!");
     }
 
-    Rect GetShadowRect(Rect baseRect, float shadowWidth, Rect flipRect)
+    /// <summary>
+    /// Releases player from wall and cleans up needed data
+    /// </summary>
+    private void ReleaseStick()
+    {
+        gameObject.transform.parent = null;
+        CharacterJoint joint = gameObject.GetComponent<CharacterJoint>();
+        if (joint != null)
+        {
+            Destroy(joint);
+        }
+        StartCoroutine(ResetStick(0.1f));
+    }
+
+    /// <summary>
+    /// Returns the Rectangle of a shadow based on given current rectangle
+    /// </summary>
+    /// <param name="baseRect"></param>
+    /// <param name="shadowWidth"></param>
+    /// <param name="flipRect"></param>
+    /// <returns></returns>
+    private Rect GetShadowRect(Rect baseRect, float shadowWidth, Rect flipRect)
     {
         return new Rect((baseRect.x - (shadowWidth * flipRect.x)), (baseRect.y - shadowWidth) * flipRect.y, (baseRect.width + ((shadowWidth * 2) * flipRect.width)), (baseRect.height + (shadowWidth * 2)) * flipRect.height);
     }
+    #endregion
 }
