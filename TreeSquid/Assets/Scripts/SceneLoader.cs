f8931a3fa847eb4f0ccb7b33a83fdabb75e6b36b
Quinn Hopwood <42815474+QuinnHop@users.noreply.github.com>
2019-10-22 20:40:31 -0400

    Added Fire to fire version of first level
    Added Fire prefab which uses the detection script to reset the player. Added fire throughout the level. Created a level loader script which loads levels when the player hits its collider. Added a shader to the exit door and scroll.

---
+++ TreeSquid/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Object sceneToLoad;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(sceneToLoad.name);
+        }
+    }
+}
