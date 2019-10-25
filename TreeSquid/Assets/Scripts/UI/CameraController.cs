7c06ed32856963e08b87aa3dce4ecf401ed320e2
Dane <dms7827@rit.edu>
2019-10-24 22:34:30 -0400

    Controls UI
    added tutorial box with fade in and out
    added slider for launch power UI

---
+++ TreeSquid/Assets/Scripts/UI/CameraController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform[] cameraPositions;
+
+    private const float lerpTime = 1;
+    private float timer = 0;
+
+    [SerializeField] private GameObject squid;
+    private Transform squidCamTransform;
+    private bool transitionedToSquid = false;
+
+    [SerializeField] private ControlsUI controlsUI;
+
+    private void Awake()
+    {
+        Transform squidCamT = squid.GetComponentInChildren<Camera>().transform;
+        squidCamTransform = new GameObject("fakeSquid").transform;
+        squidCamTransform.position = squidCamT.position;
+        squidCamTransform.rotation = squidCamT.rotation;
+    }
+
+    private void Update()
+    {
+        if (timer < lerpTime)
+        {
+            timer += Time.deltaTime;
+            if(timer > lerpTime)
+            {
+                timer = lerpTime;
+                transform.localRotation = Quaternion.identity;
+                transform.localPosition = Vector3.zero;
+
+                if (transitionedToSquid)
+                {
+                    squid.SetActive(true);
+                    controlsUI.gameObject.SetActive(true);
+
+                    gameObject.GetComponent<Camera>().enabled = (false);
+                }
+
+            }
+            else
+            {
+                float p = timer / lerpTime;
+                p = p * p * (3 - 2 * p);
+                transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.identity, p);
+                transform.localPosition = Vector3.Lerp(transform.localPosition, Vector3.zero, p);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            GoToCameraPosition(0);
+        }
+    }
+
+    //////////////////////
+    // BUTTON FUNCTIONS //
+    //////////////////////
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename, LoadSceneMode.Single);
+    }
+
+    public void GoToCameraPosition(int index)
+    {
+        if (transitionedToSquid)
+        {
+            gameObject.GetComponent<Camera>().enabled = true;
+
+            Transform squidCamT = squid.GetComponentInChildren<Camera>().transform;
+            squidCamTransform.position = squidCamT.position;
+            squidCamTransform.rotation = squidCamT.rotation;
+
+            transform.SetParent(squidCamTransform);
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+
+            squid.SetActive(false);
+            controlsUI.gameObject.SetActive(false);
+
+            transitionedToSquid = false;
+        }
+        
+        transform.SetParent(cameraPositions[index]);
+        timer = 0;
+    }
+
+    public void GoToSquid()
+    {
+        transform.SetParent(squidCamTransform);
+        timer = 0;
+
+        transitionedToSquid = true;
+    }
+}
---
+++ TreeSquid/Assets/Scripts/UI/ControlsUI.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlsUI : MonoBehaviour
+{
+    [SerializeField] private RectTransform tutTransform;
+    [SerializeField] private float showTime;
+    private Text tutText;
+    private Image tutPanel;
+
+    [SerializeField] private Slider power;
+
+    public float PowerValue { set { if (power != null) { power.value = value; } } }
+    public string TutMessage { private get; set; }
+
+    private void Awake()
+    {
+        tutText = tutTransform.GetComponentInChildren<Text>();
+        tutPanel = tutTransform.GetComponent<Image>();
+
+        StartCoroutine(Fade(tutText, 0.5f, 0, 2));
+        StartCoroutine(Fade(tutPanel, 0.5f, 0, 2));
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            ShowMessage("STOP THAT SQUID");
+        }
+    }
+
+    private IEnumerator FadeInOut(Graphic graphic, float alphaStart, float alphaFinish, float time)
+    {
+        float elapsedTime = 0;
+        Color colorStart = graphic.color;
+        colorStart.a = alphaStart;
+
+        Color colorFinish = graphic.color;
+        colorFinish.a = alphaFinish;
+
+        graphic.color = colorStart;
+
+        while (elapsedTime < time)
+        {
+            graphic.color = Color.Lerp(graphic.color, colorFinish, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        StartCoroutine(Fade(graphic, alphaFinish, alphaStart, time));
+    }
+
+    private IEnumerator Fade(Graphic graphic, float alphaStart, float alphaFinish, float time)
+    {
+        float elapsedTime = 0;
+        Color colorStart = graphic.color;
+        colorStart.a = alphaStart;
+
+        Color colorFinish = graphic.color;
+        colorFinish.a = alphaFinish;
+
+        graphic.color = colorStart;
+
+        yield return new WaitForSeconds(showTime);
+
+        while (elapsedTime < time)
+        {
+            graphic.color = Color.Lerp(graphic.color, colorFinish, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public void ShowMessage(string message)
+    {
+        TutMessage = message;
+        ShowMessage();
+    }
+
+    public void ShowMessage()
+    {
+        StopAllCoroutines();
+
+        StartCoroutine(FadeInOut(tutText, 0, 0.5f, 2));
+        StartCoroutine(FadeInOut(tutPanel, 0, 0.5f, 2));
+    }
+
+}
