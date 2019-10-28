0119fa596a24c987d6346b7cf41a41ec2d0c8c76
Andrew Brook <asb9599@rit.edu>
2019-10-28 09:44:22 -0400

    Started Line of Sight Cone

---
+++ TreeSquid/Assets/Scripts/AI/AILineOfSightCone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AILineOfSightCone : MonoBehaviour
+{
+    // Inspector Assigned Variables
+    [SerializeField] AIGuardStateMachine _guardStateMachine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if this is a 
+    }
+}
